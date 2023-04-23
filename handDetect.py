ea1fadf90f26f09ce855308f9e5dee7c744302af
Prajwal M Dixit <prajwal.dixit@guidesly.com-at-006617276998>
2023-04-23 23:50:51 +0530

    American resnet9 with morph

---
+++ handDetect.py
@@ -0,0 +1,149 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
+import numpy as np
+import math
+from model import ResNet9,to_device,get_default_device,predict_image
+import torch
+import torchvision.transforms as tt
+import mediapipe as mp
+import os
+def fun(change_pixmap_signal1,change_pixmap_signal2,run_flag,tl1,tl2):
+    mp_drawing = mp.solutions.drawing_utils
+    mp_selfie_segmentation = mp.solutions.selfie_segmentation
+    pred=[]
+
+
+    # def SegmentNorm(img):
+    #     BG_COLOR = (0,0,0)
+    #     with mp_selfie_segmentation.SelfieSegmentation(
+    #         model_selection=0) as selfie_segmentation:
+    #         image = img
+    #         results = selfie_segmentation.process(image)
+    #         condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.2
+    #         bg_image = np.zeros(image.shape, dtype=np.uint8)
+    #         bg_image[:] = BG_COLOR
+    #         imgbremoved= np.where(condition, image, bg_image)
+
+    #         transform = tt.ToTensor()
+    #         imgbremoved= transform(imgbremoved)
+
+    #         mean=imgbremoved.mean([1,2])
+    #         std=imgbremoved.std([1,2])
+    #         stats=(mean,std)
+    #         transform = tt.Compose([tt.Normalize(*stats)])
+    #         imgbremoved=transform(imgbremoved)
+    #     return imgbremoved
+
+    transform = tt.Compose([tt.ToTensor(),tt.Resize(size=(128,128))])
+
+    target_num=28
+    device = get_default_device()
+    model=ResNet9(3,28)
+    model = to_device(ResNet9(3, target_num), device)
+    model.load_state_dict(torch.load("..\\models\\ISN-4-FullGaussianMorph1500min50-custom-resnet.pth",map_location=torch.device('cpu')))
+    model.eval()
+    minValue = 50
+
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector(maxHands=1)
+
+    offset=20
+    imgSize = 400
+    while run_flag:
+        try: 
+            success, img = cap.read()
+            imgOutput = img.copy()
+            hands, img = detector.findHands(img)
+            if hands:
+                hand = hands[0]
+                x, y, w, h = hand['bbox']
+                if(w<250):
+                    w=250
+                if(h<250):
+                    h=250
+                imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)
+                imgCrop = imgOutput[y - offset:y + h + offset, x - offset-50:x + w + offset]
+
+                imgCropShape = imgCrop.shape
+
+                aspectRatio = h / w
+
+                if aspectRatio > 1:
+                    k = imgSize / h
+                    wCal = math.ceil(k * w)
+                    imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+                    imgResizeShape = imgResize.shape
+                    wGap = math.ceil((imgSize - wCal) / 2)
+                    imgWhite[:, wGap:wCal + wGap] = imgResize
+
+                    gray = cv2.cvtColor(imgWhite, cv2.COLOR_BGR2GRAY)
+                    blur = cv2.GaussianBlur(gray,(5,5),2)
+                    th3 = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,2)
+                    ret, res = cv2.threshold(th3, minValue, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+                    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
+                    res = cv2.morphologyEx(res, cv2.MORPH_OPEN, kernel)
+                    res = cv2.morphologyEx(res, cv2.MORPH_CLOSE, kernel)
+
+                    imgtensor= transform(res)
+                    predicted=predict_image(imgtensor, model)
+                    tl1.setText(predicted)
+                    pred.append(predicted)
+                    if len(pred)>=50:
+                        if pred.count(max(set(pred), key = pred.count))>35:
+                            tl2.setText(tl2.text()+max(set(pred), key = pred.count))
+                            pred.clear()
+                        else:
+                            pred.clear()
+                    
+
+                else:
+                    k = imgSize / w
+                    hCal = math.ceil(k * h)
+                    imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+                    imgResizeShape = imgResize.shape
+                    hGap = math.ceil((imgSize - hCal) / 2)
+                    imgWhite[hGap:hCal + hGap, :] = imgResize
+
+                    gray = cv2.cvtColor(imgWhite, cv2.COLOR_BGR2GRAY)
+                    blur = cv2.GaussianBlur(gray,(5,5),2)
+
+                    th3 = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,2)
+                    ret, res = cv2.threshold(th3, minValue, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+                    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
+                    res = cv2.morphologyEx(res, cv2.MORPH_OPEN, kernel)
+                    res = cv2.morphologyEx(res, cv2.MORPH_CLOSE, kernel)
+
+                    # kernel = np.zeros((3,3), np.uint8) 
+                    # # img_erosion = cv2.erode(res, kernel, iterations=2) 
+                    # res = cv2.dilate(res, kernel, iterations=5) 
+
+                    # kernel = np.ones((1,1), np.uint8) 
+                    # img_erosion = cv2.erode(res, kernel, iterations=2) 
+                    # res = cv2.dilate(img_erosion, kernel, iterations=5) 
+
+                    res= cv2.cvtColor(res, cv2.COLOR_GRAY2BGR)
+                    imgtensor= transform(res)
+                    predicted=predict_image(imgtensor, model)
+                    tl1.setText(predicted)
+                    pred.append(predicted)
+                    if len(pred)>=50:
+                        if pred.count(max(set(pred), key = pred.count))>35:
+                            tl2.setText(tl2.text()+max(set(pred), key = pred.count))
+                            pred.clear()
+                        else:
+                            pred.clear()
+                    
+
+                # crop= imgseg.permute(1,2,0).cpu().detach().numpy()
+                    
+                change_pixmap_signal2.emit(res)
+                cv2.rectangle(imgOutput, (x-offset-50, y-offset),
+                            (x + w+offset, y + h+offset), (255, 0, 255), 4)
+            
+                change_pixmap_signal1.emit(imgOutput)
+            cv2.waitKey(1)
+        except: 
+            pass
---
+++ model.py
@@ -0,0 +1,191 @@
+import os
+import torch
+import torchvision
+import tarfile
+import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
+from torchvision.datasets.utils import download_url
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader, SubsetRandomSampler
+import torchvision.transforms as tt
+from torch.utils.data import random_split
+from torchvision.utils import make_grid
+import matplotlib
+import matplotlib.pyplot as plt
+import opendatasets as od
+import cv2
+import mediapipe as mp
+import numpy as np
+from PIL import Image
+
+data_dir="..\\datasets\\segmentedImage"
+#-------------------------------------------------------------------
+stats_avgs=[0.3546225428581238, 0.31263309717178345, 0.3060757517814636]
+stats_stds=[0.21986615657806396, 0.1884787231683731, 0.16998402774333954]
+target_num=28
+#-------------------------------------------------------------------
+
+stats = (stats_avgs, stats_stds)
+train_tfms = tt.Compose([
+                         tt.RandomHorizontalFlip(), 
+
+                         tt.ToTensor(), 
+
+                         tt.Normalize(*stats,inplace=True)])
+
+valid_tfms = tt.Compose([tt.ToTensor(), tt.Normalize(*stats)])
+
+train_data = ImageFolder(data_dir, transform=train_tfms)
+valid_data = ImageFolder(data_dir, transform=valid_tfms)
+test_data = ImageFolder(data_dir, transform=valid_tfms)
+
+#-------------------------------------------------------------------
+
+num_train = len(train_data)
+indices = list(range(num_train))
+np.random.seed(42)
+np.random.shuffle(indices)
+valid_size = 0.15
+test_size = 0.10
+val_split = int(np.floor(valid_size * num_train))
+test_split = int(np.floor(test_size * num_train))
+valid_idx, test_idx, train_idx = indices[:val_split], indices[val_split:val_split+test_split], indices[val_split+test_split:]
+#-------------------------------------------------------------------
+
+batch_size = 250
+
+#------------------------------------------------------------------
+
+train_sampler = SubsetRandomSampler(train_idx)
+valid_sampler = SubsetRandomSampler(valid_idx)
+
+
+
+train_dl = torch.utils.data.DataLoader(train_data, batch_size=batch_size, 
+    sampler=train_sampler, num_workers=2, pin_memory=True)
+valid_dl = torch.utils.data.DataLoader(valid_data, batch_size=batch_size,
+    sampler=valid_sampler, num_workers=2, pin_memory=True)
+
+#------------------------------------------------------------------
+#for computing check
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+#for dataloader
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+
+#-------------------------------------------------------------------
+
+device = get_default_device()
+device
+
+#--------------------------------------------------------------------
+train_dl = DeviceDataLoader(train_dl, device)
+valid_dl = DeviceDataLoader(valid_dl, device)
+
+#---------------------------------------------------------------------
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+    
+    def validation_step(self, batch):
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+        
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_acc']))
+        
+#----------------------------------------------------------------------------------------------------------
+
+def conv_block(in_channels, out_channels, pool=False):
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), 
+              nn.BatchNorm2d(out_channels), 
+              nn.ReLU(inplace=True)]
+    if pool: layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
+#model defenition
+class ResNet9(ImageClassificationBase):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        
+        self.conv1 = conv_block(in_channels, 64, pool=True) 
+        self.conv2 = conv_block(64, 128, pool=True) 
+        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128)) 
+        self.conv3 = conv_block(128, 256, pool=True) 
+        self.conv4 = conv_block(256, 512, pool=True) 
+        self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512)) 
+        self.conv5 = conv_block(512, 512, pool=True) 
+        self.classifier = nn.Sequential(nn.MaxPool2d(4), 
+                                        nn.Flatten(), 
+                                        nn.Dropout(0.2),
+                                        nn.Linear(512, num_classes))
+        
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out
+        out = self.conv5(out)
+        out = self.classifier(out)
+        return out
+#------------------------------------------------------------------------------------------------
+
+model = to_device(ResNet9(3, target_num), device)
+
+#-------------------------------------------------------------------------------
+
+def predict_image(img, model):
+    # Convert to a batch of 1
+    xb = to_device(img.unsqueeze(0), device)
+    # Get predictions from model
+    yb = model(xb)
+    # Pick index with highest probability
+    _, preds  = torch.max(yb, dim=1)
+    # Retrieve the class label
+    return train_data.classes[preds[0].item()]

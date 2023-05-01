41270955600317152973c94cb6b30c5a5184cedd
Prajwal M Dixit <prajwal.dixit@guidesly.com-at-006617276998>
2023-05-01 11:30:17 +0530

    Mediapipe deletd

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import opendatasets as od
 import cv2
-import mediapipe as mp
+#import mediapipe as mp
 import numpy as np
 from PIL import Image
 
